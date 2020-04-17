58c60c63e48e131beb437587bef116f943693fac
Benedek Burgess <benedekb97@gmail.com>
2020-04-18 01:20:02 +0200

    Fix typo

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-5">
             <div class="card">
                 <div class="card-body">
-                    <p style="text-align:justify;">Ezen az oldalon megjelenített "kártyák" nem vélemény-nyilvánítások, vagy bármi ahhoz hasonló. Csupán a véletlen műve, hogy ezeket a kártyákat üsszerakták az emberek, meg talán azért mert vicces.</p>
+                    <p style="text-align:justify;">Ezen az oldalon megjelenített "kártyák" nem vélemény-nyilvánítások, vagy bármi ahhoz hasonló. Csupán a véletlen műve, hogy ezeket a kártyákat összerakták az emberek, meg talán azért mert vicces.</p>
                     <p style="text-align:justify;">Don't be a dick. Nem szidunk senkit, ha komolyan veszed az itteni "poénokat" akkor ne nézegesd az oldalt.</p>
                     <p>Pá</p>
                 </div>
